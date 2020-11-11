35cf0f3a12915c26952ca043186739643867377c
李满满 <limanman@corp.netease.com>
2020-11-11 12:06:56 +0800

    :underage:

---
+++ namekox_security/constants.py
@@ -0,0 +1,9 @@
+#! -*- coding: utf-8 -*-
+
+# author: forcemain@163.com
+
+
+SECURITY_CONFIG_KEY = 'SECURITY'
+DEFAULT_SECURITY_JWT_SECRET = 'namekox'
+DEFAULT_SECURITY_JWT_MAXAGE = 2 * 60 * 60
+DEFAULT_SECURITY_JWT_EXPIRE = 24 * 60 * 60
